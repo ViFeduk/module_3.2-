f3498f3f6053832f502e22bbbfcc734c97a7b903
ViFeduk <viktor2005fedchenkov@mail.ru>
2024-02-04 17:29:10 +0300

    Добавьте файлы проекта.

---
+++ 3.2 lessons/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _3._2_lessons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Viktor";
+            
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
